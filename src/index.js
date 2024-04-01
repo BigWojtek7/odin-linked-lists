5c9027916cc4f6ef7c2b8046a9c1555e49e59810
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-04-01 08:31:26 +0200

    Add classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+class LinkedList{
+  constructor(name) {
+    this.name = name;
+  }
+
+  append(value){
+    ...
+  }
+  prepend(value){
+    ...
+  }
+}
+
+class Node {
+  constructor(value = null , nextNode = null){
+    this.value = value
+    this.nextNode = nextNode
+  }
+}
