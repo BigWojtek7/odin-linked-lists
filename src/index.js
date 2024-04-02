697045ec8e202d79be693d45f09f3b289a15c5d5
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-04-02 11:13:37 +0200

    Add additional methods to linked-list-class

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ list.append(5555);
 list.append(4320);
 list.append(7777);
 list.append(2323);
+list.prepend("first");
 console.dir(list, { depth: null });
+console.log(list.getHead())
--- src/linked-list-class.js
+++ src/linked-list-class.js
@@ -3,6 +3,7 @@ import Node from "./node-class.js";
 class LinkedList {
   constructor(head = null) {
     this.head = head;
+    this.size = 0;
   }
 
   append(value) {
@@ -21,11 +22,31 @@ class LinkedList {
 
       current.nextNode = node;
     }
+    this.size += 1
   }
 
-  // prepend(value){
-  //   ...
-  // }
+  prepend(value){
+    const node = new Node(value);
+
+    let current;
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = node;
+      current.nextNode = this.head
+      this.head = current;
+    }
+    this.size += 1
+  }
+
+  getSize(){
+    return this.size
+  }
+
+  getHead(){
+    return Object.values(this.head)[0]
+  }
 }
 
 export default LinkedList;
